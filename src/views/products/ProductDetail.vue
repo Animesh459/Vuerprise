9020a2a706a824f4072695f8b501743a64701a71
animesh shuvro <animeshshuvro@gmail.com>
2025-12-23 08:38:22 +0600

    compile error on product details fixed

--- src/views/products/ProductDetail.vue
+++ src/views/products/ProductDetail.vue
@@ -12,8 +12,6 @@
 import ProductInfo from "@/components/product/ProductInfo.vue";
 import Inventory from "@/components/product/Inventory.vue";
 import UploadImageVideo from "@/components/product/UploadImageVideo.vue";
-import Demo from "@/components/product/Demo.vue";
-import Demo2 from "@/components/product/Demo2.vue";
 
 </script>
 
